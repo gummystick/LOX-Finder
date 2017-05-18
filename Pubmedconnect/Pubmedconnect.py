d0c7a52f969152d499f09a0e7646f6da67b0e806
gummystick <gummystick@users.noreply.github.com>
2017-05-18 09:50:50 +0200

    Update Pubmedconnect.py

--- Pubmedconnect/Pubmedconnect.py
+++ Pubmedconnect/Pubmedconnect.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from Bio import Entrez
+import datetime
 
 app = Flask(__name__)
 
@@ -9,10 +11,28 @@ def hello_world():
     jaartal=request.args.get("Jaartal")
     return render_template('./index.html')
 
-@app.route('/test')
-def test():
-    return render_template('./test.html')
+@app.route('/table', methods=["GET"])
+def table():
+    eiwit = request.args.get("Eiwit")
+    jaartal = request.args.get("Jaartal")
+    ids = searchids(eiwit, jaartal)
+    links = linking(ids)
+    return render_template('./table.html', eiwit=eiwit, jaartal=str(jaartal), ids=str(ids), links=links)
 
+def searchids(eiwit, jaartal):
+    Entrez.email = "W.Sies@han.nl"
+    date2 = str(int(str(datetime.datetime.today())[0:4])+1)
+    readhandle = Entrez.read(
+        Entrez.esearch(db="pubmed", term=str(eiwit) + " AND {0}:{1} [PDAT]".format(jaartal, date2), datetype="pdat",
+                       usehistory="y"))
+    ids = readhandle.get('IdList')
+    return ids
+
+def linking(idlist):
+    linklist = []
+    for id in idlist:
+        linklist.append(str(id))
+    return linklist
 
 if __name__ == '__main__':
     app.run()
