fe3982fc3bf0a0d0f0f7d22b8353d51e44e566ff
rubenV252 <ruben.vermaas1@gmail.com>
2017-06-07 22:37:55 +0200

    Textming update

--- Pubmedconnect/Pubmedconnect.py
+++ Pubmedconnect/Pubmedconnect.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from Bio import Entrez, Medline
+from nltk import word_tokenize
 import datetime
 
 app = Flask(__name__)
@@ -9,16 +10,23 @@ app = Flask(__name__)
 def hello_world():
     eiwit=request.args.get("Eiwit")
     jaartal=request.args.get("Jaartal")
-    return render_template('./index.html')
+    return render_template('index.html')
 
 @app.route('/table', methods=["GET"])
 def table():
     eiwit = request.args.get("Eiwit")
     jaartal = request.args.get("Jaartal")
     rows = searchids(eiwit, jaartal)
-    return render_template('./table.html', eiwit=eiwit, jaartal=str(jaartal), newrows=rows)
+    return render_template('table.html', eiwit=eiwit, jaartal=str(jaartal), newrows=rows)
 
 def searchids(eiwit, jaartal):
+
+    classification = ['plants', 'animals', 'bacteria', 'fungi']
+    key_words = ['lipoxygenase', 'Bleaching', 'hydroperoxide', 'oleochemistry', 'oxylipin', 'fatty acids','application', 'oxidation', 'biosynthesis']
+
+    rijk = []
+    key = []
+
     Entrez.email = "W.Sies@han.nl"
     date2 = str(int(str(datetime.datetime.today())[0:4])+1)
     readhandle = Entrez.read(
@@ -32,13 +40,32 @@ def searchids(eiwit, jaartal):
     for artikel in openArtikels:
         count += 1
         abstract = artikel.get("AB", "-")
+        words = word_tokenize(abstract)
+        for i in words:
+            if i in classification:
+                rijk.append(i)
+            elif i in key_words:
+                key.append(i)
         author = artikel.get("AU", "-")
         dateOfPublish = artikel.get("DP", "-")
         publicationType = artikel.get("PT", "-")
         pmid = artikel.get("PMID", "-")
-        keywords = artikel.get("OT", "-")
         title = artikel.get("TI", "-")
-        newRow = "<tr><td><a href=""https://www.ncbi.nlm.nih.gov/pubmed?term="+str(ids[count])+">"+str(ids[count])+"</td><td>"+title+"</td><td>"+",".join(author)+"</td><td>"+dateOfPublish+"</td><td>"+"\n".join(keywords)+"</td><td>"+"".join(abstract)+"</td></tr>"
+        twords = word_tokenize(title)
+        for i in twords:
+            if i in classification:
+                rijk.append(i)
+            elif i in key_words:
+                key.append(i)
+
+        rijk1 = set(rijk)
+        rijk2 = list(rijk1)
+
+        key1 = set(key)
+        key2 = list(key1)
+
+        newRow = "<tr><td><a href=""https://www.ncbi.nlm.nih.gov/pubmed?term="+str(ids[count])+">"+str(ids[count])+"</td><td>"+title+"</td><td>"+",".join(author)+"</td><td>"+dateOfPublish+"</td><td>"+"\n".join(rijk2)+"</td><td>"+"\n".join(key2)+"</td><td>"+"".join(abstract)+"</td></tr>"
+
     return newRow
 
 if __name__ == '__main__':
