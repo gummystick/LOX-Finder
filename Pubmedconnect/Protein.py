7e129b0a8b857c7e178f22e0d78c2a8f05c4163e
gummystick <willysieswilly@gmail.com>
2017-06-08 13:00:09 +0200

    protein class added

---
+++ Pubmedconnect/Protein.py
@@ -0,0 +1,24 @@
+class Protein:
+    def __init__(self):
+        self.taxonomy = []
+        self.assecions = []
+        self.references = []
+    def setTaxonomy(self, taxonomy):
+        self.taxonomy = taxonomy
+        return
+
+    def setAssecions(self, assecions):
+        self.assecions = assecions
+        return
+
+    def setReferences(self, references):
+        self.references = references
+
+    def getTaxonomy(self):
+        return self.taxonomy
+
+    def getAssecions(self):
+        return self.assecions
+
+    def getReferences(self):
+        return self.references
