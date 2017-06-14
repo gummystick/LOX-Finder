ae9b8031dec9b3484ee3dce398815e9519e190e2
gummystick <willysieswilly@gmail.com>
2017-06-14 13:59:06 +0200

    Merge
    # Conflicts:
    #	.idea/LOX-Finder.iml
    #	.idea/misc.xml
    #	.idea/modules.xml
    #	Pubmedconnect/Pubmedconnect.py
    #	Pubmedconnect/templates/table.html

--- Pubmedconnect/Tabel.py
+++ Pubmedconnect/Tabel.py
@@ -1,7 +1,8 @@
 import DBConnect
-
+#Tabel class. Deze class is voor het verzamelen en doorsturen van de tabel data.
 class Tabel:
-
+    #Initiator. Slaat de parameters: eiwit en jaartal op in class variabele.
+    #De variabele tabelArt en tabelProt zijn voor de opslag van de artikel en protein data uit de classes Artikel en Protein.
     def __init__(self, eiwit, jaartal):
         self.DBConnect = DBConnect
         self.eiwit = eiwit
@@ -9,7 +10,9 @@ class Tabel:
         self.tabelArt = ""
         self.tabelProt = ""
 
-
+    #Methode om de tabel data te verkrijgen door de DBConnect class. De connect.searchArtikel zoekt artikelen
+    #de methode connect.compareArtikels vergelijkt de artikelen op keywords en de comparedArtikelSearch zoekt voor de tweede keer de pubmed database door.
+    #zie ook de class DBConnect. connect.searchProtein zoekt naar artikelen in de Protein database.
     def getTable(self):
         connect = DBConnect.DBConnect(self.eiwit, self.jaartal)
         connect.searchArtikels()
