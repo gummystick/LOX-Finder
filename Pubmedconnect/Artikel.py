c7bf58bdac1fb414bf16b1584d4790354ba8f82f
gummystick <willysieswilly@gmail.com>
2017-06-14 13:42:44 +0200

    Merge
    # Conflicts:
    #	.idea/LOX-Finder.iml
    #	.idea/misc.xml
    #	.idea/modules.xml
    #	Pubmedconnect/Pubmedconnect.py
    #	Pubmedconnect/templates/table.html

--- Pubmedconnect/Artikel.py
+++ Pubmedconnect/Artikel.py
@@ -1,5 +1,10 @@
+#Python class voor het opslaan van pubmed Artikel data.
 class Artikel:
-
+    #Initiator van de Artikel class.
+    #Parameters: Abstract(Samenvatting), author(auteur), dateOfPublish(datum van publicatie),
+    # pmid(pubmed ID), keywords(belangrijke woorden), title(titel)
+    #Parameters worden opgeslagen in class variabelen.
+    #Tablerow is de variabele voor het opslaan van de tabel data voor visualisatie.
     def __init__(self, abstract, author, dateOfPublish, publicationType, pmid, keywords, title):
         self.abstract = abstract
         self.author = author
@@ -10,55 +15,50 @@ class Artikel:
         self.title = title
         self.tableRow = ""
 
-    def setAbstract(self, abstract):
-        self.abstract = abstract
-        return
-
-    def setAuthor(self, author):
-        self.author = author
-        return
-
-    def setDateOfPublish(self, dateOfPublish):
-        self.dateOfPublish = dateOfPublish
-
-    def setPublicationType(self, publicationType):
-        self.PublicationType = publicationType
-
-    def setPmid(self, pmid):
-        self.pmid = pmid
-
-    def setKeywords(self, keywords):
-        self.keywords = keywords
-
-    def setTitle(self, title):
-        self.title = title
-
+    #setTableRow:
+    #zet de data van het artikel in html tabel vorm.
     def setTableRow(self):
         self.tableRow += "<tr><td><a href=""https://www.ncbi.nlm.nih.gov/pubmed?term=" + str(self.pmid) + ">" + str(
             self.pmid) + "</td><td>" + self.title + "</td><td>" + ",".join(
             self.author) + "</td><td>" + self.dateOfPublish + "</td><td>" + "\n".join(
             self.keywords) + "</td><td>" + "".join(self.abstract) + "</td></tr>"
 
+    #getTableRow:
+    #haalt de gegevens van tableRow op en returnt deze.
     def getTableRow(self):
         return self.tableRow
 
+    #getAbstract:
+    #haalt de gegevens van Abstract op en returnt deze.
     def getAbstract(self):
         return self.abstract
 
+    #getAuthor:
+    #haalt de gegevens van Auteur op en returnt deze.
     def getAuthor(self):
         return self.author
 
+    #getDateOfPublish:
+    #haalt de gegevens van dateOfPublish op en returnt deze.
     def getDateOfPublish(self):
         return self.dateOfPublish
 
+    #getPublicationType:
+    #haalt de gegevens van publicationType op en returnt deze.
     def getPublicationType(self):
         return self.publicationType
 
+    #getPmid:
+    #haalt de gegevens van pmid op en returnt deze.
     def getPmid(self):
         return self.pmid
 
+    #getKeywords:
+    #haalt de gegevens van keywords op en returnt deze.
     def getKeywords(self):
         return self.keywords
 
+    #getTitle:
+    #haalt de gegevens van title op en returnt deze.
     def getTitle(self):
         return self.title
